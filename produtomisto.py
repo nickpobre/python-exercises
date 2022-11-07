091284c07d0564db91d7eb9a1d6a02ff831fd51d
Felipe O de Andrade <41307625+nickpobre@users.noreply.github.com>
2022-11-07 13:48:31 -0300

    Update produtomisto.py

--- produtomisto.py
+++ produtomisto.py
@@ -1,6 +1,15 @@
+
 import PySimpleGUI as sg
 #define um tema para a parte visual
 sg.theme("Reddit")
+#Função que calcula o produto vetorial
+def produto_vetorial(vetor = []):
+    w1 = vetor[0]*vetor[3]
+    w2 = vetor[1]*vetor[4] 
+    w3 = vetor[2]*vetor[5]
+    f = [w1,w2,w3]
+    return f
+
 #Função que calcula o produto misto
 def produtomisto(vetor = []):  
     
@@ -20,6 +29,7 @@ def produtomisto(vetor = []):
     #caso não seja coplanar ele retorna o valor do produto final
     else:
         return produto
+
 #Cria a primeira janela
 def make_win1():
     #layout da primeira tela
@@ -36,6 +46,7 @@ def make_win1():
             [sg.Button('Proximo'), sg.Button('Sair')]
         ]
     return sg.Window('Definição produto misto', layout= Layout, finalize=True)
+
 #cria a janela 2
 def make_win2():
     #layout da segunda janela
@@ -44,9 +55,9 @@ def make_win2():
         [sg.Button("Retornar"),sg.Button("Proximo"),sg.Button("Sair")]
     ]
     return sg.Window('Parte 2', layout= Layout, finalize=True)
+
 #Cria a janela 3
 def make_win3():
-    tela_r = sg.Text(key="-r-")
     #layout da terceira janela
     Layout = [
         [sg.Text("Digite os valores dos vetores abaixo")],
@@ -57,14 +68,33 @@ def make_win3():
         [sg.Text("Vetor W:")],
         [sg.Input(do_not_clear=True,size=(10,1),key='-7-'),sg.Input(do_not_clear=True,size=(10,1),key='-8-'),sg.Input(do_not_clear=True,size=(10,1),key='-9-')],
         [sg.Text("Resultado:"),tela_r],
-        [sg.Button("Calcular",key='-c-'),sg.Button('Retornar'),sg.Exit("Sair")]
+        [sg.Button("Calcular",key='-c-'),sg.Button('Retornar'),sg.Button("Proximo"),sg.Exit("Sair")]
     ]
-    return sg.Window('Window Title', layout = Layout, finalize=True)
+    return sg.Window('Calcular produto misto', layout = Layout, finalize=True)
+
+#Cria a janela 4
+def make_win4():
+    Layout = [
+        [sg.Text("Digite os valores dos vetores abaixo")],
+        [sg.Text("Vetor U:")],
+        [sg.Input(do_not_clear=True,size=(10,1),key='-1-'),sg.Input(do_not_clear=True,size=(10,1),key='-2-'),sg.Input(do_not_clear=True,size=(10,1),key='-3-')],
+        [sg.Text("Vetor V:")],
+        [sg.Input(do_not_clear=True,size=(10,1),key='-4-'),sg.Input(do_not_clear=True,size=(10,1),key='-5-'),sg.Input(do_not_clear=True,size=(10,1),key='-6-')],
+        [sg.Text("Resultado:"),tela_r],
+        [sg.Text("Produto Misto:"),sg.Text(key='-pmr-')],
+        [sg.Button("Calcular",key='-pv-'),sg.Button('Retornar'),sg.Exit("Sair")]
+    ]
+    return sg.Window('Calcular produto vetorial', layout = Layout, finalize=True)
+
 #deifine a janela inicial
-window1, window2 , window3 = make_win1(), None, None
+window1, window2 , window3, window4 = make_win1(), None, None, None
+
 #loop
-while True:             
+while True:
+
     window, event, Values = sg.read_all_windows()
+    tela_r = sg.Text(key="-r-")
+    
     #caso aperte no botão de sair
     if event == sg.WIN_CLOSED or event == 'Sair':
         window.close()
@@ -72,23 +102,35 @@ while True:
             window2 = None
         elif window == window1:
             break
+
     #Caso esteja na janela 1 e aperta no botão proximo ele esconde a janela 1 e mostra a janela 2
     elif window == window1 and event == 'Proximo':
         window2 = make_win2()
         window1.hide()
-        #Caso esteja na janela 2 e aperta no botão proximo ele esconde a janela 2 e mostra a janela 3
+    
+    #Caso esteja na janela 2 e aperta no botão proximo ele esconde a janela 2 e mostra a janela 3
     elif window == window2 and event == 'Proximo':
         window3 = make_win3()
         window2.hide()
+    
+    elif window == window3 and event == 'Proximo':
+        window4 = make_win4()
+        window3.hide()
+    
     #Caso esteja na janela 2 e aperte em retornar ele esconde a janela 2 e mostra a janela 1
     if window == window2 and event == 'Retornar':
         window1.un_hide()
         window2.hide()
+    
     #Caso esteja na janela 3 e aperte em retornar ele esconde a janela 3 e mostra a janela 2
     if window == window3 and event == 'Retornar':
         window2.un_hide()
         window3.hide()
-    #caso aperte o botão calcular ele pega os valores e envia para a função produtomisto() /linha 5/
+    if window == window4 and event == 'Retornar':
+        window3.un_hide()
+        window4.hide()
+    
+    #caso aperte o botão calcular ele pega os valores e envia para a função produtomisto()
     if event == '-c-':
         vet = [
             int(Values['-1-']),
@@ -104,5 +146,24 @@ while True:
         resultado = produtomisto(vet)
         #mostra na tela o resultado
         window['-r-'].update(resultado)
+    
+    #caso aperte o botão calcular ele pega os valores e envia para a função produto_vetorial()
+    if event == '-pv-':
+        vet = [
+            int(Values['-1-']),
+            int(Values['-2-']),
+            int(Values['-3-']),
+            int(Values['-4-']),
+            int(Values['-5-']),
+            int(Values['-6-']),
+        ]
+        resultado = produto_vetorial(vet)
+        new_array = []
+        new_array.append(vet)
+        new_array.append(resultado)
+        a = new_array[0]+new_array[1]
+        b = produtomisto(a)
+        window['-r-'].update(resultado)
+        window['-pmr-'].update(b)
 
 window.close()
